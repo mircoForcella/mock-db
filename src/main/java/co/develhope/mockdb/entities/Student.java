7de5bf0b81004c4944603f876689d747e39ca894
mircoForcella <forcella1996@gmail.com>
2023-05-06 17:04:58 +0200

    Initial commit

---
+++ src/main/java/co/develhope/mockdb/entities/Student.java
@@ -0,0 +1,25 @@
+package co.develhope.mockdb.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Table
+@AllArgsConstructor
+@NoArgsConstructor
+public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    @Column(nullable = false)
+    private String firstName;
+    @Column(nullable = false)
+    private String lastName;
+    @Column(unique = true)
+    private String email;
+}
